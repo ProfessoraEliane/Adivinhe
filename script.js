e6424634174b07eb0082dcea8d0dd19c767b9e2c
Elianne Márcia MUdrei <101529296+ProfessoraEliane@users.noreply.github.com>
2022-09-10 11:36:21 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+var numeroSecreto = parseInt(Math.random() * 11);
+
+function Chutar() {
+  var chute = parseInt(document.getElementById("valor").value);
+  var elementoResultado = document.getElementById("resultado");
+
+  if (chute === numeroSecreto) {
+    elementoResultado.innerHTML = "Você acertou";
+  } else if (chute > 10 || chute < 0) {
+    elementoResultado.innerHTML = "Você deve digitar um número de 0 a 10";
+  } else if (numeroSecreto > chute) {
+    elementoResultado.innerHTML = "O número deve ser maior";
+  } else if (numeroSecreto < chute) {
+    elementoResultado.innerHTML = "O número deve ser menor";
+  }
+}
